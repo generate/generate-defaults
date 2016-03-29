34e556ba3a93fc1c53e208abfe35d48e9ec0d6c7
jonschlinkert <jon.schlinkert@sellside.com>
2016-03-29 11:27:12 -0400

    refactor
    still trying to figure out the best way to do these defaults. might change later

--- test.js
+++ test.js
@@ -9,34 +9,12 @@ var app;
 describe('generate-defaults', function() {
   beforeEach(function() {
     app = generate();
+    app.use(defaults);
   });
 
-  describe('task', function() {
-    it('should work as a task', function(cb) {
-      app.task('foo', defaults.task(app));
-      app.build('foo', function(err) {
-        if (err) return cb(err);
-        cb();
-      });
-    });
-  });
-
-  describe('load', function() {
-    describe('collections', function() {
-      it('should add collections to the instance', function(cb) {
-        defaults.invoke(app);
-
-        assert.equal(typeof app.docs, 'function');
-        assert.equal(typeof app.includes, 'function');
-        assert.equal(typeof app.layouts, 'function');
-        cb();
-      });
-    });
-
+  describe('plugin', function() {
     describe('engine', function() {
       it('should add an engine to the instance', function(cb) {
-        defaults.invoke(app);
-
         assert(app.engines['.*']);
         assert.equal(typeof app.engines['.*'], 'object');
         assert.equal(typeof app.engines['.*'].render, 'function');
@@ -46,26 +24,26 @@ describe('generate-defaults', function() {
   });
 
   describe('generator', function() {
-    it('should extend a generator', function(cb) {
+    it('should add defaults to a generator', function() {
+      app.use(defaults);
+      assert(app.cache.data.hasOwnProperty('runner'));
+      assert(app.cache.data.hasOwnProperty('name'));
+      assert(app.cache.data.hasOwnProperty('version'));
+    });
+
+    it('should add defaults to a sub-generator', function(cb) {
+      app.use(defaults);
+      var count = 0;
+
       app.generator('foo', function(foo) {
-        foo.extendWith(defaults);
-        var count = 0;
-
-        foo.task('bar', function(next) {
-          assert(foo.views.hasOwnProperty('docs'));
-          assert(foo.views.hasOwnProperty('layouts'));
-          assert(foo.views.hasOwnProperty('includes'));
-          count++;
-          next();
-        });
-
-        foo.task('default', ['defaults', 'bar']);
-        foo.build('default', function(err) {
-          if (err) return cb(err);
-          assert.equal(count, 1);
-          cb();
-        });
+        assert(foo.cache.data.hasOwnProperty('runner'));
+        assert(foo.cache.data.hasOwnProperty('name'));
+        assert(foo.cache.data.hasOwnProperty('version'));
+        count++;
       });
+
+      assert.equal(count, 1);
+      cb();
     });
   });
 });
