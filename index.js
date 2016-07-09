d8ceb4a7a07d30d5ec534600274bad44cc5ac797
jonschlinkert <jon.schlinkert@sellside.com>
2016-07-09 00:06:56 -0400

    rename file

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
 'use strict';
 
-module.exports = require('./generator');
+var os = require('os');
+var fs = require('fs');
+var path = require('path');
+var utils = require('./utils');
+
+module.exports = function plugin(app, base) {
+  if (!utils.isValid(app, 'generate-defaults')) return;
+
+  /**
+   * Plugins
+   */
+
+  app.use(utils.pkg());
+  app.use(utils.middleware());
+  app.use(utils.questions());
+  app.use(require('verb-repo-data'));
+
+  /**
+   * Engine
+   */
+
+  if (!app.getEngine('*')) {
+    app.engine('*', require('engine-base'), app.option('engineOpts'));
+  }
+
+  /**
+   * Helpers
+   */
+
+  app.helpers(require('template-helpers')());
+
+  /**
+   * Data
+   */
+
+  var projectData = utils.merge({}, app.pkg.data, app.get('cache.data.project'));
+  app.data({project: projectData});
+  return plugin;
+};
