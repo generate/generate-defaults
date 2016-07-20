7b26955f3f9e93f6076f465e057943d11ffb7840
jonschlinkert <jon.schlinkert@sellside.com>
2016-07-20 18:20:42 -0400

    adds `check-directory` task

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var utils = require('./utils');
+var answer;
 
 module.exports = function plugin(app) {
   if (!utils.isValid(app, 'generate-defaults')) return;
@@ -40,5 +41,34 @@ module.exports = function plugin(app) {
    */
 
   app.helpers(require('template-helpers')());
+
+  /**
+   * Tasks
+   */
+
+  app.task('check-directory', function(cb) {
+    if (utils.isEmpty(app.cwd) || answer === true) {
+      cb();
+      return;
+    }
+
+    app.confirm('directory', 'The current working directory has existing files in it, are you sure you want to proceed?', {
+      default: false
+    });
+    app.ask('directory', function(err, answers) {
+      if (err) {
+        cb(err);
+        return;
+      }
+      if (answers.directory === false) {
+        app.log.warn(app.log.yellow('Got it, exiting process'));
+        process.exit();
+      } else {
+        answer = true;
+        cb();
+      }
+    });
+  });
+
   return plugin;
 };
